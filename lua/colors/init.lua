9402008ddd8c3396d956a4e7c298f4b4269ddab0
benjaminPla <benjaminpla.dev@gmail.com>
2026-03-27 15:14:07 +0100

    create theme_2, add Theme command

--- lua/colors/init.lua
+++ lua/colors/init.lua
@@ -1,5 +1,32 @@
-local palette = require('colors.theme_0')
+local M = {}
+local highlights = require('colors.highlights')
 
-require('colors.highlights').apply(palette)
+local themes = {
+  [0] = 'colors.theme_0',
+  [1] = 'colors.theme_1',
+  [2] = 'colors.theme_2',
+}
+
+function M.switch(n)
+  local mod = themes[tonumber(n)]
+  if not mod then
+    vim.notify('Theme ' .. n .. ' not found. Available: 0, 1, 2', vim.log.levels.ERROR)
+    return
+  end
+  -- Bust the require cache so re-sourcing works cleanly
+  package.loaded[mod] = nil
+  local palette = require(mod)
+  highlights.apply(palette)
+  vim.notify('Switched to theme ' .. n, vim.log.levels.INFO)
+end
+
+-- :Theme <n>
+vim.api.nvim_create_user_command('Theme', function(opts)
+  M.switch(opts.args)
+end, { nargs = 1 })
+
+-- Load default theme on startup
+local palette = require(themes[0])
+highlights.apply(palette)
 
 return palette
---
+++ lua/colors/theme_2.lua
@@ -0,0 +1,41 @@
+return {
+  -- Backgrounds (dark green-black, phosphor monitor vibes)
+  bg            = "#060c06",
+  bg_dark       = "#040904",
+  panel         = "#020602",
+  cursorline    = "#0d1a0d",
+  visual_bg     = "#0f1f0f",
+  visual_select = "#122612",
+
+  -- Foreground
+  fg            = "#a8ff8a",   -- soft phosphor green
+  faded         = "#2d5c2d",   -- dark muted green
+  comment       = "#3a6e3a",   -- medium-dark green
+
+  -- Syntax
+  keyword       = "#ff2d6a",   -- hot pink
+  func          = "#00ff88",   -- bright mint green
+  string        = "#39d353",   -- medium green
+  number        = "#ccff44",   -- acid chartreuse
+  type          = "#00e5ff",   -- cyan (breaks up the green)
+  operator      = "#ff6eb4",   -- soft pink
+  constant      = "#aaff00",   -- lime green
+  variable      = "#a8ff8a",   -- same as fg
+  special       = "#00ffaa",   -- aquamarine
+  exception     = "#ff3333",   -- bright red
+
+  -- Diagnostics
+  error         = "#ff2222",
+  warning       = "#ffaa00",
+  info          = "#00e5ff",
+  hint          = "#00cc77",
+
+  -- Diff
+  diff_add      = "#0a2a0a",   -- deep green bg
+  diff_delete   = "#2a0808",   -- deep red bg
+  diff_change   = "#0a1a1a",   -- deep teal bg
+  diff_text     = "#142828",   -- slightly lighter teal
+
+  -- Misc
+  black         = "#000000",
+}
